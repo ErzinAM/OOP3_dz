d3daef23c6125fbb126c479c0b5576ff9ac00b73
ErzinAM <150605651+ErzinAM@users.noreply.github.com>
2024-03-23 18:45:12 +0300

    StudentComparator.java

---
+++ StudentComparator.java
@@ -0,0 +1,10 @@
+public class StudentComparator implements Comparator<Student>{
+
+    @Override
+    public int compare(Student arg0, Student arg1) {
+        String name0 = arg0.getSurname() + arg0.getFirstname() + arg0.getPatronymic();
+        String name1 = arg1.getSurname() + arg1.getFirstname() + arg1.getPatronymic();
+        return name0.compareToIgnoreCase(name1);        
+    }
+    
+}
