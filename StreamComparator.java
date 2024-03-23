45d2fc204dce4e27b8fee9ea28596d82c4b7d72b
ErzinAM <150605651+ErzinAM@users.noreply.github.com>
2024-03-23 18:42:33 +0300

    StreamComparator.java

---
+++ StreamComparator.java
@@ -0,0 +1,8 @@
+public class StreamComparator implements Comparator<Stream>{
+
+    @Override
+    public int compare(Stream arg0, Stream arg1) {
+        return Integer.compare(arg0.getListStudentGroup().size(), arg1.getListStudentGroup().size());
+    }
+    
+}
